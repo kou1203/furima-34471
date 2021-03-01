0beeadc0fe194504120dfbf6478af079989ab4ea
kou1203 <78337586+kou1203@users.noreply.github.com>
2021-03-01 12:15:40 +0900

    出品機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,28 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
 
   def index 
+    @products = Product.order("created_at DESC")
   end
+
+  def new 
+    @product = Product.new
+  end 
+
+  def create 
+    @product = Product.new(product_params)
+      if @product.save
+        redirect_to root_path
+      else  
+        render :new 
+      end 
+  end 
+
+  private
+
+  def product_params 
+    params.require(:product).permit(:image, :name, :description, :category_id,
+    :status_id, :shipping_burden_id, :prefecture_id, :days_to_ship_id, :price ).merge(user_id: current_user.id )
+  end 
 
 end

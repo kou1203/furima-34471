289d3c809e1a4fe6528d03d54e612fc945ee192a
kou1203 <78337586+kou1203@users.noreply.github.com>
2021-03-03 11:41:39 +0900

    purchase product_historyモデル実装

---
+++ db/migrate/20210303021026_create_product_histories.rb
@@ -0,0 +1,9 @@
+class CreateProductHistories < ActiveRecord::Migration[6.0]
+  def change
+    create_table :product_histories do |t|
+      t.references :user,    foreign_key: true 
+      t.references :product, foreign_key: true
+      t.timestamps
+    end
+  end
+end
